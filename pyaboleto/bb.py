6a0ea030ebac1eefc27508ed65c52f0890c48d35
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2015-07-14 13:40:23 -0300

    Cobertura de teste 100% para o Banco do Brasil.

--- pyaboleto/bb.py
+++ pyaboleto/bb.py
@@ -31,6 +31,14 @@ banco_brasil = Banco('001', 'Banco do Brasil')
 
 class BBBoleto(Boleto):
 
+    def _validar_convenio(self, tam_conv, tam_nos_num, masc_exp=', 1 hífen e 1 dv', masc_dv='-0'):
+        if not re.match('^\d{%d}$' % tam_conv, self.convenio.numero):
+            raise PYABoletoExcpetion('Para convênio de %d posições o número do convênio deve conter '
+                                     'apenas dígitos (%s)' % (tam_conv, str.zfill('0', tam_conv)))
+        if not re.match('^\d{%d}\-\d$' % tam_nos_num, self.nosso_numero):
+            raise PYABoletoExcpetion('Para convênio de %d posições o Nosso Número deve conter '
+                                     '%d dígitos%s (0000000000%s)' % (tam_conv, tam_nos_num, masc_exp, masc_dv))
+
     def _validar_codigo_barras(self):
         """
         Como a classe ancestral já valida os campos padronizados pela FEBRABAN basta
@@ -38,26 +46,51 @@ class BBBoleto(Boleto):
         """
         super(BBBoleto, self)._validar_codigo_barras()
 
-        if not re.match('\d{3}', self.convenio.carteira):
-            raise PYABoletoExcpetion('A carteira deve conter 3 digitos (000)')
+        self._validar_digitos(self.convenio.carteira, 2, 'carteira')
+        self._validar_digitos(self.convenio.agencia, 4, 'agência', True)
+        self._validar_digitos(self.convenio.conta, 8, 'agência', True)
 
-        if not re.match('\d{4}\-\d', self.convenio.agencia):
-            raise PYABoletoExcpetion('A agência deve conter 4 digitos, 1 hífen e 1 dv (0000-0)')
+        if len(self.convenio.numero) not in (4, 6, 7):
+            raise PYABoletoExcpetion('O número do convênio deve ter 4 (0000), 6 (000000) ou 7 dígitos (0000000)')
 
-        if not re.match('\d{5}\-\d', self.convenio.conta):
-            raise PYABoletoExcpetion('A conta deve conter 5 digitos, 1 hífen e 1 dv (00000-0)')
+        tam_conv = len(self.convenio.numero)
+        if tam_conv == 4:
+            self._validar_convenio(tam_conv, 7)
+        elif tam_conv == 6:
+            if self.convenio.carteira == '21':
+                if not re.match('^\d{17}$', self.nosso_numero):
+                    raise PYABoletoExcpetion('Para Carteiras 16 e 18 (Tipo de Modalidade de Cobrança 21) '
+                                             'o Número do Convênio deve ter 6 posições e o '
+                                             'Nosso Número 17 dígitos sem hífen e sem 1 dv (00000000000000000)')
+            else:
+                self._validar_convenio(tam_conv, 5)
+        elif tam_conv == 7:
+            self._validar_convenio(tam_conv, 10)
 
     @property
     def campo_livre(self):
         """
         Gera o campo livre do código de barras.
         """
-        agencia_conta = "%4s%5s" % (self.convenio.agencia[:-2], self.convenio.conta[:-2])
-        campo_livre = "%3s%s%1s%4s%5s%1s000" % (self.convenio.carteira,
-                                                self.nosso_numero,
-                                                modulo10("%s%3s%8s" %
-                                                         (agencia_conta, self.convenio.carteira, self.nosso_numero)),
-                                                self.convenio.agencia[:-2],
-                                                self.convenio.conta[:-2],
-                                                modulo10(agencia_conta))
-        return campo_livre
+        self._validar_codigo_barras()
+        sem_dv = -2
+        if len(self.convenio.numero) == 4:
+            return '%4s%7s%4s%8s%2s' % (self.convenio.numero,
+                                        self.nosso_numero[:sem_dv],
+                                        self.convenio.agencia[:sem_dv],
+                                        self.convenio.conta[:sem_dv],
+                                        self.convenio.carteira)
+        elif len(self.convenio.numero) == 6 and self.convenio.carteira != '21':
+            return '%6s%5s%4s%8s%2s' % (self.convenio.numero,
+                                        self.nosso_numero[:sem_dv],
+                                        self.convenio.agencia[:sem_dv],
+                                        self.convenio.conta[:sem_dv],
+                                        self.convenio.carteira)
+        elif len(self.convenio.numero) == 6 and self.convenio.carteira == '21':
+            return '%6s%17s%2s' % (self.convenio.numero,
+                                   self.nosso_numero,
+                                   self.convenio.carteira)
+        elif len(self.convenio.numero) == 7:
+            return '000000%7s%10s%2s' % (self.convenio.numero,
+                                         self.nosso_numero[:sem_dv],
+                                         self.convenio.carteira)
--- tests/test_base.py
+++ tests/test_base.py
@@ -27,8 +27,11 @@ import unittest
 from datetime import date
 import pyaboleto
 import pyaboleto.itau
+import pyaboleto.bb
 
-DATA_TESTE = date(2015, 7, 6)
+
+DATA_TESTE = date(2007, 12, 31)
+
 
 class BaseTestCase(unittest.TestCase):
     def _test_padrao(self):
@@ -39,6 +42,17 @@ class BaseTestCase(unittest.TestCase):
 
 
 class RootTestCase(BaseTestCase):
+
+    def test_modulo11(self):
+        self.assertEqual('x', pyaboleto.modulo11('6', 'x'))
+        self.assertEqual('x', pyaboleto.modulo11('0', 'z', 'x'))
+        self.assertEqual('0-x', pyaboleto.modulo11('0', 'z', 'x', True))
+        self.assertEqual('0=x', pyaboleto.modulo11('0', 'z', 'x', True, separador='='))
+
+    def test_fator_vencimento(self):
+        con = pyaboleto.Convenio('7777777', '22', pyaboleto.Banco('333', 'FEBRABAN'), '5555-5', '66666-6')
+        bol = pyaboleto.Boleto('07006700', DATA_TESTE, 34.75, con, None, None)
+        self.assertEqual('3737', bol.fator_vencimento)
 
     def test_caminho_feliz(self):
         end, sac, ced = self._test_padrao()
@@ -73,60 +87,88 @@ class RootTestCase(BaseTestCase):
         sac_str = 'Nome do cidadão - 123.456.789-01 - 123465 SSP/UF - %s' % end_str
         ced_str = 'Nome da empresa - 12.345.678/9012-34 - %s' % end_str
         con_str = '7777777 - 22, 333 - 5555-5 - 66666-6'
-        bol_str = '33390.00006 00000.000000 00000.000000 1 64810000003475'
+        bol_str = '33390.00006 00000.000000 00000.000000 4 37370000003475'
 
         end, sac, ced = self._test_padrao()
-        self.assertEqual(str(end), end_str)
-        self.assertEqual(str(sac), sac_str)
-        self.assertEqual(str(ced), ced_str)
+        self.assertEqual(end_str, str(end))
+        self.assertEqual(sac_str, str(sac))
+        self.assertEqual(ced_str, str(ced))
         end.complemento = None
-        self.assertEqual(str(end), end_str2)
+        self.assertEqual(end_str2, str(end))
 
         con = pyaboleto.Convenio('7777777', '22', pyaboleto.Banco('333', 'FEBRABAN'), '5555-5', '66666-6')
-        self.assertEqual(str(con), con_str)
+        self.assertEqual(con_str, str(con))
 
         bol = pyaboleto.Boleto('07006700', DATA_TESTE, 34.75, con, None, None)
-        self.assertEqual(str(bol), bol_str)
+        self.assertEqual(bol_str, str(bol))
 
     def test_implementacao_errada(self):
         class FailBoleto(pyaboleto.Boleto):
+            @property
             def campo_livre(self):
                 return str.zfill('0', 24)
         con = pyaboleto.Convenio('7777777', '22', pyaboleto.Banco('333', 'FEBRABAN'), '5555-5', '66666-6')
         bol = FailBoleto('07006700', DATA_TESTE, 34.75, con, None, None)
         with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, 'O campo livre deve conter 25 digito.*'):
-            tmp = bol.codigo_barras
-
-    def test_modulo11(self):
-        self.assertEqual(pyaboleto.modulo11('6', 'x'), 'x')
-        self.assertEqual(pyaboleto.modulo11('0', 'z', 'x'), 'x')
-        self.assertEqual(pyaboleto.modulo11('0', 'z', 'x', True), '0-x')
-        self.assertEqual(pyaboleto.modulo11('0', 'z', 'x', True, separador='='), '0=x')
+            self.assertEqual(bol.codigo_barras, '33391648100000034750000000000000000000000000')
 
 class ItauTestCase(BaseTestCase):
     def test_caminho_feliz(self):
         con = pyaboleto.Convenio(None, '109', pyaboleto.itau.banco_itau, '9314-9', '32857-7')
         bol = pyaboleto.itau.ItauBoleto('07006700', DATA_TESTE, 34.75, con, None, None)
 
-        self.assertEqual(bol.campo_livre, '1090700670069314328577000')
-        self.assertEqual(bol.codigo_barras, '34191648100000034751090700670069314328577000')
-        self.assertEqual(bol.linha_digitavel, '34191.09073 00670.069319 43285.770004 1 64810000003475')
+        self.assertEqual('1090700670069314328577000', bol.campo_livre)
+        self.assertEqual('34193373700000034751090700670069314328577000', bol.codigo_barras)
+        self.assertEqual('34191.09073 00670.069319 43285.770004 3 37370000003475', bol.linha_digitavel)
 
     def test_carteira_errada(self):
-        from pyaboleto.itau import ItauBoleto, banco_itau
         con = pyaboleto.Convenio('123456', '21', pyaboleto.itau.banco_itau, '1234-5', '12345-6')
         bol = pyaboleto.itau.ItauBoleto('07006700', DATA_TESTE, 34.75, con, None, None)
-        with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, 'A carteira deve conter.*'):
+        with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, '.*carteira deve conter.*'):
             bol._validar_codigo_barras()
 
     def test_agencia_errada(self):
         con = pyaboleto.Convenio('123456', '109', pyaboleto.itau.banco_itau, '1234', '12345')
         bol = pyaboleto.itau.ItauBoleto('07006700', DATA_TESTE, 34.75, con, None, None)
-        with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, 'A agência deve conter.*'):
+        with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, '.*agência deve conter.*'):
             bol._validar_codigo_barras()
 
     def test_conta_errada(self):
         con = pyaboleto.Convenio('123456', '109', pyaboleto.itau.banco_itau, '1234-5', '12345')
         bol = pyaboleto.itau.ItauBoleto('07006700', DATA_TESTE, 34.75, con, None, None)
-        with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, 'A conta deve conter.*'):
+        with self.assertRaisesRegexp(pyaboleto.PYABoletoExcpetion, '.*conta deve conter.*'):
             bol._validar_codigo_barras()
+
+
+class BBTestCase(BaseTestCase):
+    def test_caminho_feliz_convenio4(self):
+        con = pyaboleto.Convenio('5094', '31', pyaboleto.bb.banco_brasil, '1606-0', '06809350-0')
+        bol = pyaboleto.bb.BBBoleto('1234567-0', DATA_TESTE, 1.0, con, None, None)
+
+        self.assertEqual('5094123456716060680935031', bol.campo_livre)
+        self.assertEqual('00197373700000001005094123456716060680935031', bol.codigo_barras)
+        self.assertEqual('00195.09413 23456.716069 06809.350314 7 37370000000100', bol.linha_digitavel)
+
+    def test_caminho_feliz_convenio6(self):
+        con = pyaboleto.Convenio('050094', '31', pyaboleto.bb.banco_brasil, '1606-0', '06809350-0')
+        bol = pyaboleto.bb.BBBoleto('01448-0', DATA_TESTE, 1.0, con, None, None)
+
+        self.assertEqual('0500940144816060680935031', bol.campo_livre)
+        self.assertEqual('00193373700000001000500940144816060680935031', bol.codigo_barras)
+        self.assertEqual('00190.50095 40144.816069 06809.350314 3 37370000000100', bol.linha_digitavel)
+
+    def test_caminho_feliz_convenio6_carteira21(self):
+        con = pyaboleto.Convenio('050094', '21', pyaboleto.bb.banco_brasil, '1606-0', '06809350-0')
+        bol = pyaboleto.bb.BBBoleto('12345678901234567', DATA_TESTE, 1.0, con, None, None)
+
+        self.assertEqual('0500941234567890123456721', bol.campo_livre)
+        self.assertEqual('00196373700000001000500941234567890123456721', bol.codigo_barras)
+        self.assertEqual('00190.50095 41234.567893 01234.567210 6 37370000000100', bol.linha_digitavel)
+
+    def test_caminho_feliz_convenio7(self):
+        con = pyaboleto.Convenio('1050094', '31', pyaboleto.bb.banco_brasil, '1606-0', '06809350-0')
+        bol = pyaboleto.bb.BBBoleto('1234657890-0', DATA_TESTE, 1.0, con, None, None)
+
+        self.assertEqual('0000001050094123465789031', bol.campo_livre)
+        self.assertEqual('00192373700000001000000001050094123465789031', bol.codigo_barras)
+        self.assertEqual('00190.00009 01050.094125 34657.890314 2 37370000000100', bol.linha_digitavel)
