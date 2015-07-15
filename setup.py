6b56ccfb31427d8a58b6e2d5237bb5263bef93fb
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2015-07-15 11:04:37 -0300

    Preparando os arquivos para enviar para o PyPI

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='pyaboleto',
+    packages=['pyaboleto'], # this must be the same as the name above
+    version='0.1',
+    description='O pyaboleto e um componente de boleto bancario em Python que oferece um conjunto de classes para gerar'
+                ' os dados dos boletos e não os templates. O pyaboleto é uma reescrita do YaBoleto PHP desenvolvido'
+                ' pela umbrelaTech.',
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    url='https://github.com/umbrellaTech/pyaboleto', # use the URL to the github repo
+    download_url='https://github.com/umbrellaTech/pyaboleto/tarball/0.1', # I'll explain this in a second
+    keywords=['boleto'], # arbitrary keywords
+    classifiers=[],
+)
